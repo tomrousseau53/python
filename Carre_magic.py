dd796161756a515c1948e30d1a6037cc9f13e3bd
tomrousseau53 <102742354+tomrousseau53@users.noreply.github.com>
2022-03-31 16:57:03 +0200

    Add files via upload

--- Carre_magic.py
+++ Carre_magic.py
@@ -1,12 +1,17 @@
 from pycsp3 import *
+from tkinter import *
 
 def genere_carre_magic():
+    
     n = int(input("Donner valeur n: "))
 
+    #Formule du TP à vérifier
     sum = int(n*(n*n+1)/2)
 
+    #Définition du tableau
     x = VarArray(size=[n, n], dom=range(1, n*n+1))
 
+    #Vérification des conditions pour le carré magique
     satisfy(
         AllDifferent(x),
         [Sum(row) == sum for row in x],
@@ -22,12 +27,27 @@ def genere_carre_magic():
             print(values(x[i]))
         print("Number of solutions: ", n_solutions())
         print("Domain of any variable: ", x[0][0].dom)
-
+        
+        #Affichage du carré magique
+        window = Tk()
+        window.title("Carré magique: ")
+        for row in range (n):
+            for col in range (n):
+                Button(window, text=x[row][col].grid(row=row, column =col))
+        window.mainloop()
+               
+    
+    
 def genere_carre_bimagic():
     n = int(input("Donner valeur n: "))
+    
+    #Formule du carré magique
     sum = int(n*(n*n+1)/2)
+    
+    #Formule du carré bimagic à vérifier en plus
     sumSquared = int(n*(n*n+1)*(2*n*n+1)/6)
     
+    #Grille de sudoku
     clues = [
         [0, 2, 0, 5, 0, 1, 0, 9, 0],
         [8, 0, 0, 2, 0, 3, 0, 0, 6],
@@ -40,16 +60,25 @@ def genere_carre_bimagic():
         [0, 1, 0, 9, 0, 7, 0, 6, 0]
     ]
     
+    #Définition du tableau
     x = VarArray(size=[n, n], dom=range(1, n*n+1))
     
+    #Vérification du carré bimagic
     satisfy(
-        AllDifferent(x),
+         AllDifferent(x),
         [x[i][j] == clues[i][j] for i in range(9) for j in range(9)
-     if clues and clues[i][j] > 0],
+     if clues and clues[i][j] > 0], #Condition pour remplir la grille de Sudoku
+        
+        #Condition carré magique
         [Sum(row) == sum for row in x],
         [Sum(col) == sum for col in columns(x)],
+        
+        #Diagonale à partir de la case (0,0)
         [Sum(x[i, i] for i in range(n)) == sum],
+        #Diagonale à partir de la case (i,i)
         [Sum(x[i, n-i-1] for i in range(n)) == sum],
+        
+        #Condition à vérifier en plus pour carré bimagic
         [Sum(x[i, j]*x[i, j] for i in range(n) for j in range(n)) == sumSquared],
         [Sum(x[j, i]*x[j, i] for i in range(n) for j in range(n)) == sumSquared],
         [Sum(x[i, i]*x[i, i] for i in range(n)) == sumSquared],
@@ -64,7 +93,9 @@ def genere_carre_bimagic():
         print("Domain of any variable: ", x[0][0].dom)
     else:
         print("No solutions for bimagic")
- 
+    
+
+#Execution du programme 
 if __name__ == "__main__":
     genere_carre_magic()
     genere_carre_bimagic()
